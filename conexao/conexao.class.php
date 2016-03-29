947c435642df56cd268326528b408bf30a6ec103
Bryan Alves <info.bryanalves@gmail.com>
2016-03-28 21:41:46 -0300

    Merge origin/master
    Conflicts:
    	index.php

---
+++ conexao/conexao.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class conexao {
+
+    private static $host = 'localhost';
+    private static $user = 'root';
+    private static $pass = '';
+    private static $db = 'mar-de-sabores';
+    private static $connect = null;
+
+    private static function conectar() {
+        try {
+            if (self::$connect == null) {
+                $dsn = 'mysql:host=' . self::$host . ';dbname=' . self::$db;
+                $options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'];
+                self::$connect = new PDO($dsn, self::$user, self::$pass, $options);
+            }
+        } catch (Exception $e) {
+            echo $e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine();
+            die();
+        }
+        self::$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return self::$connect;
+    }
+
+    public static function getConexao() {
+        return self::conectar();
+    }
+
+}
