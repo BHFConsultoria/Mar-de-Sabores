51413d809f2b7eca0c973cd8d0d376edc4040853
Bryan Alves <info.bryanalves@gmail.com>
2016-05-13 22:08:22 -0300

    Adaptação do código com o design

--- bo/LoginBO.class.php
+++ bo/LoginBO.class.php
@@ -1,38 +1,56 @@
 <?php
 
 class LoginBO {
-    
+
     private $dao;
-    
-    private $nmEmail;
-    private $dsSenha;
-    
+
     function __construct() {
         $this->dao = new LoginDAO();
     }
 
-    function verificaLogin($nmEmail,$dsSenha,$tpUsuario){        
-        $this->setNmEmail($nmEmail);
-        $this->setDsSenha($dsSenha);
-        
-        $resultado = $this->dao->verificaLogin($this->nmEmail, $this->dsSenha,$tpUsuario);
-        
-        if(empty($resultado)){
-            echo "usuário nao encontrado";
-        }else{
-            echo "usuário encontrado";
-            var_dump(md5($resultado[0]['ds_senha']));
+    function verificaLogin($nmEmail, $dsSenha, $tpUsuario) {
+
+        if ($tpUsuario == 'confeiteiro') {
+            $bean = new ConfeiteiroBean();
+        } else if ($tpUsuario == 'cliente') {
+            $bean = new ClienteBean();
         }
-        
-    }
-    
-    function setNmEmail($nmEmail) {
-        $this->nmEmail = $nmEmail;      
-    }
 
-    function setDsSenha($dsSenha) {
-        $this->dsSenha = $dsSenha;
-    }
+        $bean->setNmEmail($nmEmail);
+        $bean->setDsSenha($dsSenha);
 
+        $resultado = $this->dao->verificaLogin($bean->getNmEmail(), $bean->getDsSenha(), $tpUsuario);
+
+        if (empty($resultado)) {
+            echo "usuário nao encontrado";
+        } else {
+            session_start();
+
+            $_SESSION['codigo'] = ($tpUsuario == 'confeiteiro') ? $resultado[0]['cd_confeiteiro'] : $resultado[0]['cd_cliente'] ;
+            $_SESSION['nome'] = ($tpUsuario == 'confeiteiro') ? $resultado[0]['nm_confeiteiro'] : $resultado[0]['nm_cliente'];
+            $_SESSION['nmEmail'] = $resultado[0]['nm_email'];
+            $_SESSION['dsSenha'] = $resultado[0]['ds_senha'];
+            $_SESSION['nmRazaoSocial'] = $resultado[0]['nm_razao_social'];
+            $_SESSION['nmFantasia'] = $resultado[0]['nm_fantasia'];
+            $_SESSION['cdCpf'] = $resultado[0]['cd_cpf'];
+            $_SESSION['cdCnpj'] = $resultado[0]['cd_cnpj'];
+            $_SESSION['cdInscricaoEstadual'] = $resultado[0]['cd_inscricao_estadual'];
+            $_SESSION['dtNascimento'] = $resultado[0]['dt_nascimento'];
+            $_SESSION['cdTelefone'] = $resultado[0]['cd_telefone'];
+            $_SESSION['cdCelular'] = $resultado[0]['cd_celular'];
+            $_SESSION['nmLogradouro'] = $resultado[0]['nm_logradouro'];
+            $_SESSION['nmComplemento'] = $resultado[0]['nm_complemento'];
+            $_SESSION['nmCidade'] = $resultado[0]['nm_cidade'];
+            $_SESSION['nmBairro'] = $resultado[0]['nm_bairro'];
+            $_SESSION['cdCep'] = $resultado[0]['cd_cep'];
+            $_SESSION['sgUf'] = $resultado[0]['sg_uf'];
+            $_SESSION['sgSexo'] = $resultado[0]['sg_sexo'];
+            
+            /*
+             * Preciso ajustar isso!!!
+             */
+            header('Location: ../../view/confeiteiro/indexConfeiteiro.php');
+        }
+    }
 
 }
