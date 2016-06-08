276d5fbf3fb460fad515a0823778e87b1b310c9c
Flavia <flaviaass65@gmail.com>
2016-06-08 17:30:56 -0300

    Merge origin/master
    Conflicts:
    	view/bootstrap/css/style.css
    	view/confeiteiro/indexConfeiteiro.php

--- view/produto/produtoView.php
+++ view/produto/produtoView.php
@@ -1,61 +1,65 @@
 <?php
 include_once '../../controller/produto/produtoController.php';
+session_start();
 ?>
 <!DOCTYPE html>
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . 'Mar-de-Sabores/Mar-de-Sabores/cabecalhoConfeiteiro.php'; ?>
-        <!-- /Section: intro -->
-        <!-- Section: Area Cadastro Produto -->
-        <section id="produto" class="home-section text-left">
-		<div class="heading-contact">
-			<div class="container">
-			<div class="row">
-				<div class="col-lg-8 col-md-offset-2">
-					
-					<div class="section-heading">
-					<div class="wow bounceInDown" data-wow-delay="0.4s">
-					<!--<h2>Produtos</h2>-->
-					</div>
-					</div>
-					
-				</div>
-			</div>
-			</div>
-		</div>
-            <div class="container" id="produto">
+<?php include_once './cabecalhoProduto.php'; ?> 
+<!-- /Section: intro -->
+<!-- Section: Area Cadastro Produto -->
+<section id="produto" class="home-section text-left">
+    <div class="heading-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-offset-2">
 
-    <div class="row">
-        <div class="col-lg-6 col-md-6">
-            <img src="Mar-de-Sabores/Mar-de-Sabores/bootstrap/view/img/bolo-decorado-chocolate.jpg">
+                    <div class="section-heading">
+                        <div class="wow bounceInDown" data-wow-delay="0.4s">
+                            <!--<h2>Produtos</h2>-->
+                        </div>
+                    </div>
 
-    </div>	
-         <div class="col-lg-6 col-md-6" id="">
-            <h2 class="text-center">Bolo de Chocolate</h2>
-             <p>
-Código do Produto: 002<br>
-Categoria: Bolos - Bolos Chocolate<br>
-<b>Valor: R$ 49,00</b>
-             </p>
-             <p><b>Formas de Pagamento</b></p>
-             <img src="../img/bandeiras.jpg">
-    </div>	
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" id="produto">
 
-		</div>
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <img src="../../view/bootstrap/img/bolo-decorado-chocolate.jpg">
+
+            </div>	
+            <div class="col-lg-6 col-md-6" id="">
+                
+                    <h2 class="text-center"><?php $_SESSION['nmProduto'] ?> </h2>
+                    <p>
+                        Código do Produto: 002<br>
+                        Categoria: Bolos - Bolos Chocolate<br>
+                        <b>Valor: R$ 49,00</b>
+                    </p>
+                    <p><b>Formas de Pagamento</b></p>
+                    <img src="../../view/bootstrap/img/bandeiras.jpg">
+
+                </div>	
+
+            </div>
             <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <p>
-            <b>Descrição do Produto</b><br>
-Bolo de Chocolate com Recheio de Brigadeiro e Cobertura de Brigadeiro com Granulado.
-</p>
-<p>
-<b>Pedido Mínimo:</b><br> 2kg. Bolos acima de 3kg serão em forma quadrada.
-</p>
-<b>Quantidade Recomendada:</b><br> 100g para cada 1 pessoa
+                        <b>Descrição do Produto</b><br>
+                        Bolo de Chocolate com Recheio de Brigadeiro e Cobertura de Brigadeiro com Granulado.
+                    </p>
+                    <p>
+                        <b>Pedido Mínimo:</b><br> 2kg. Bolos acima de 3kg serão em forma quadrada.
+                    </p>
+                    <b>Quantidade Recomendada:</b><br> 100g para cada 1 pessoa
 
-    </div>	
+                </div>	
             </div>
-                        </div>
-	</section>
-	<!-- /Section: Formulario PF -->
+        </div>
+    
+    </section>
+    <!-- /Section: Formulario PF -->
 
-	
-<?php include_once $_SERVER['DOCUMENT_ROOT'] . 'Mar-de-Sabores/Mar-de-Sabores/rodape.php'; ?>
+
+    <?php include_once '../../view/produto/rodape.php'; ?> 
